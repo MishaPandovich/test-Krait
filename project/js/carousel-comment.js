35ee7b91366b86abdcf7bc0c19316e09d700c6aa
Виктория <Ovcharka@gmail.com>
2019-02-01 23:44:11 +0300

    промежуточное сохранение

---
+++ project/js/carousel-comment.js
@@ -0,0 +1,43 @@
+var slideWidth = 300; 
+   
+$('.slidewrapper').width($('.slidewrapper').children().length * slideWidth);
+
+$('#next_slide').click(nextSlide);
+$('#prev_slide').click(prevSlide);
+
+function nextSlide(){
+  var hr = document.querySelector('.slide-active');
+  var slide = document.querySelectorAll('.slide');
+  hr.classList.remove('slide-active');
+  hr.nextElementSibling.classList.add('slide-active');
+
+    var currentSlide=parseInt($('.slidewrapper').data('current'));
+    currentSlide++;
+
+    if(currentSlide>=$('.slidewrapper').children().length - 2)
+    {
+        $('.slidewrapper').css('left',-(currentSlide-1)*slideWidth);  
+        $('.slidewrapper').append($('.slidewrapper').children().first().clone()); 
+        $('.slidewrapper').children().first().remove();
+        currentSlide--;                        
+    }
+ 
+    $('.slidewrapper').animate({left: -currentSlide*slideWidth},300).data('current',currentSlide);
+}
+function prevSlide(){
+  var hr = document.querySelector('.slide-active');
+  var slide = document.querySelectorAll('.slide');
+  hr.classList.remove('slide-active');
+  hr.previousElementSibling.classList.add('slide-active');
+
+    var currentSlide=parseInt($('.slidewrapper').data('current'));
+    currentSlide--;
+    if(currentSlide<0)
+    {
+        $('.slidewrapper').css('left',-(currentSlide+1)*slideWidth);  
+        $('.slidewrapper').prepend($('.slidewrapper').children().last().clone()); 
+        $('.slidewrapper').children().last().remove();
+        currentSlide++;   
+    }
+    $('.slidewrapper').animate({left: -currentSlide*slideWidth},300).data('current',currentSlide);
+}
